bd77526d95406b4d3616f54273f72369a2a72c64
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2025-03-16 22:16:55 +0000

    add STATIC_ROOT

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.x/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.x/ref/settings/#default-auto-field
