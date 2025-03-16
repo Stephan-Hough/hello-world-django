c0a098e5f95f8ae9e500459e7a216c2ca8ee86de
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2025-03-16 20:15:57 +0000

    remove localhost

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-18-170-90-71.eu-west-2.compute.amazonaws.com', 'hough.cc', 'localhost']
+ALLOWED_HOSTS = ['ec2-18-170-90-71.eu-west-2.compute.amazonaws.com', 'hough.cc']
 
 # Application definition
 
